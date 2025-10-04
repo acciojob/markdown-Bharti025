ba4b4e41429a63982393b714bd22b58c7de64518
bharti025 <20001003032bharti@dcrustm.org>
2025-10-04 12:40:35 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,28 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React,{useState,useEffect} from 'react'
+import '../styles/App.css'
+const App = () => {
+    const [text,setText]=useState("");
+    const handleChange=(e)=>{
+        setText(e.target.value);
+    }
+   useEffect(()=>{
+    let output= document.getElementsByClassName("preview");
+    output[0].innerText=text;
+   },[text]);
+
+  return (
+    <div className='app'>
+    <div className='input'>
+     <textarea className='textarea' onChange={handleChange}></textarea>
+    </div>
+    <div className='preview'></div>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
 
